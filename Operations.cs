4a9169fd79c094c1e51b52907be9d9691fbbf83a
Илья Троненко <BigWander@mail.ru>
2022-12-20 20:35:05 +0700

    Добавьте файлы проекта.

---
+++ Operations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DeleteConsole
+{
+    internal class Operations
+    {
+        public static Dictionary<string, string> LogicalOperations = new Dictionary<string, string>()
+        {
+            { "and", "192" },
+            { "xor", "60" },
+            { "or", "252" },
+            { "->", "207" },
+            { "~", "195"}
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CA_3_Final;
+using System.Linq;
+using System;
+
+namespace CA_3_Final
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логическое выражение: ");
+            string logicalInput = Console.ReadLine();
+
+            Console.WriteLine(new CellMachine().Cycle(logicalInput));
+
+
+            Console.WriteLine("Введите числа для расчета суммы: ");
+            string input = Console.ReadLine();
+
+            var numbers = input.Split("+").Select(number => number.ToEightBitBinary()).ToArray();
+
+            var summator = new Summator(numbers);
+            summator.Work();
+
+            Console.WriteLine(summator.Result);
+        }
+    }
+}
